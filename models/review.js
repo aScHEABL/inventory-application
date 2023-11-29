44c82b3047cb35aca94c77fba65fb71a8c5ad556
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-11-29 17:26:04 +0800

    fix: comments to comment

--- models/review.js
+++ models/review.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
     rating: { type: Number, required: true },
-    comments: { type: String },
+    comment: { type: String },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     cloth: { type: mongoose.Schema.Types.ObjectId, ref: 'Cloth' },
 });
