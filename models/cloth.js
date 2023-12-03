f6f2b01565fddb1f70353741fe6df313c6341fb4
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-12-03 21:26:54 +0800

    WIP: experimental change #0

--- models/cloth.js
+++ models/cloth.js
@@ -7,17 +7,13 @@ const ClothSchema = new Schema({
     price: { type: Number, required: true },
     description: { type: String, required: true },
     gender: { type: String, required: true },
-    size: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Size', required: true }],
-    color: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Color', required: true }],
+    size: { type: mongoose.Schema.Types.ObjectId, ref: 'Size', required: true },
+    color: { type: mongoose.Schema.Types.ObjectId, ref: 'Color', required: true },
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
   });
 
 ClothSchema.virtual("url").get(function() {
     return `/inventory/cloth/${this._id}`;
-})
-
-ClothSchema.virtual("stock").get(function() {
-    return 
 })
 
 module.exports = mongoose.model("Cloth", ClothSchema);
