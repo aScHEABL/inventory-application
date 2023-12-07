e35adc30811208bc25bcbf0ec13aff4637540664
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-12-07 17:31:57 +0800

    WIP: import function from controller

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 // Require controller modules.
+const clothController = require("../controllers/clothController");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home Page' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Home Page' });
+// });
 
+router.get("/", clothController.index);
 module.exports = router;
