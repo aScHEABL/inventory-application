87226e24e8e00ae19b501b8f146481d38b77c2a0
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-12-08 02:09:44 +0800

    update: random select 4 cloth

--- controllers/clothController.js
+++ controllers/clothController.js
@@ -1,14 +1,17 @@
 const Cloth = require("../models/cloth");
 const ClothInstance = require("../models/clothInstance");
 
+const getRandom = require("../utils/getRandom");
+
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async(req, res, next) => {
     const allClothes_array = await Cloth.find({}).exec();
 
+    const featuredProducts_array = getRandom(allClothes_array, 4);
     res.render("index",
         {
             title: 'Home Page',
-            clothesArray: allClothes_array,
+            clothes_Array: featuredProducts_array,
         })
 })
