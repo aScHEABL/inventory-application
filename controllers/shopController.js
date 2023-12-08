1973b4fc6988cad8415275d00353bd2b895aae7b
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-12-08 17:23:55 +0800

    update: pass gender and category array to view

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,11 +1,16 @@
 const Cloth = require("../models/cloth");
 const Category = require("../models/category");
+const Gender = require("../models/gender");
 
 const asyncHandler = require("express-async-handler");
 
 exports.main = asyncHandler(async(req, res, next) => {
+    const allGenders_array = await Gender.find({}).exec();
+    const allCategories_array = await Category.find({}).exec();
     res.render("shop",
         {
-            title: 'Shop Page1',
+            title: 'Shop Page',
+            genders_array: allGenders_array,
+            categories_array: allCategories_array,
         })
 })
