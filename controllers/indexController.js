a5ff5d464e333787c32c9ac539936ed33c0fbc4c
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-12-08 14:39:14 +0800

    update: controller base on route rather than model

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,17 +1,16 @@
 const Cloth = require("../models/cloth");
-const ClothInstance = require("../models/clothInstance");
 
 const getRandom = require("../utils/getRandom");
 
 const asyncHandler = require("express-async-handler");
 
-exports.index = asyncHandler(async(req, res, next) => {
+exports.featuredProducts = asyncHandler(async(req, res, next) => {
     const allClothes_array = await Cloth.find({}).exec();
 
     const featuredProducts_array = getRandom(allClothes_array, 4);
     res.render("index",
         {
-            title: 'Home Page',
+            title: 'Fake Fashion E-Store',
             clothes_Array: featuredProducts_array,
         })
 })
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // Require controller modules.
-const clothController = require("../controllers/clothController");
+const indexController = require("../controllers/indexController");
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Home Page' });
-// });
-
-router.get("/", clothController.index);
+router.get("/", indexController.featuredProducts);
 module.exports = router;
