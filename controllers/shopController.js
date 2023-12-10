2595f04f0bce255f6faaf644a37fcb8de192fd8e
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-12-10 20:20:55 +0800

    update: pass category base on gender schema

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -6,12 +6,20 @@ const asyncHandler = require("express-async-handler");
 
 exports.main = asyncHandler(async(req, res, next) => {
     const allGenders_array = await Gender.find({}).exec();
+    const maleGender = await Gender.findOne({ name: "male" }).exec();
+    const femaleGender = await Gender.findOne({ name: "female" }).exec();
+
+    const maleCategories_array = await Category.find({ gender: maleGender }).exec();
+    const femaleCategories_array = await Category.find({ gender: femaleGender }).exec();
+    
     const allCategories_array = await Category.find({}).exec();
 
     res.render("shop",
         {
             title: 'Shop Page',
             genders_array: allGenders_array,
+            maleCategories_array: maleCategories_array,
+            femaleCategories_array: femaleCategories_array,
             categories_array: allCategories_array,
         })
 })
