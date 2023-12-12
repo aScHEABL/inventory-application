2b35072c3cd1bbe87929b048ad6f16593be0fa42
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-12-12 17:27:43 +0800

    wip: pass cloth filter base on gender

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -8,14 +8,13 @@ exports.main = asyncHandler(async(req, res, next) => {
     const allGenders_array = await Gender.find({}).exec();
     const maleGender = await Gender.findOne({ name: "male" }).exec();
     const femaleGender = await Gender.findOne({ name: "female" }).exec();
+    
 
     const maleCategories_array = await Category.find({ gender: maleGender }).exec();
     const femaleCategories_array = await Category.find({ gender: femaleGender }).exec();
 
-    const maleClothesPromises = maleCategories_array.map(async (item, index) => {
-        return await Cloth.find({ category: item }).exec();
-    })
-    const maleClothes_array = await Promise.all(maleClothesPromises);
+    const maleClothes_array = await Promise.all(maleCategories_array.map(async (item) => await Cloth.find({ category: item }).exec()));
+    const femaleClothes_array = await Promise.all(femaleCategories_array.map(async (item) => await Cloth.find({ category: item }).exec()));
     
     const allCategories_array = await Category.find({}).exec();
 
@@ -26,6 +25,7 @@ exports.main = asyncHandler(async(req, res, next) => {
             maleCategories_array: maleCategories_array,
             femaleCategories_array: femaleCategories_array,
             maleClothes_array: maleClothes_array,
+            femaleClothes_array: femaleClothes_array,
             categories_array: allCategories_array,
         })
 })
