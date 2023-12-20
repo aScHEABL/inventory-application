d4bb000a0ba6c0f7702cc7b3347c6b2f859ae36b
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-12-20 08:11:19 +0800

    update: change type to name

--- models/size.js
+++ models/size.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const SizeSchema = new Schema({
-    type: {
+    name: {
         type: String,
         enum: ['small', 'medium', 'large', 'extra-large',],
     },
