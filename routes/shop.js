2248aed7914f396f2a5759cdab69094a7497b431
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2024-01-08 17:25:08 +0800

    fix: git merge conflict

--- routes/shop.js
+++ routes/shop.js
@@ -8,14 +8,11 @@ router.get("/", shopController.main);
 
 router.get("/inventory/cloth/:id", shopController.cloth_details);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 router.get("/inventory/overview", shopController.inventory_overview)
-=======
+
 router.get("/inventory/create/cloth", shopController.cloth_create_get);
->>>>>>> 8e83c5e6d54bd6b065f7805026435ce819c78097
-=======
+
 router.get("/inventory/create/cloth", shopController.cloth_create_get);
->>>>>>> 8e83c5e6d54bd6b065f7805026435ce819c78097
+
 
 module.exports = router;
