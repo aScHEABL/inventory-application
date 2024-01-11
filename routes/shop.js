0c201bc38f83987cc4dbab400987f3ff1d93c14a
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2024-01-11 17:25:24 +0800

    wip: clothing overview page

--- routes/shop.js
+++ routes/shop.js
@@ -8,11 +8,11 @@ router.get("/", shopController.main);
 
 router.get("/inventory/clothing/:id", shopController.clothing_details);
 
-router.get("/inventory/overview", shopController.inventory_overview)
+router.get("/inventory/overview", shopController.inventory_overview);
 
-router.get("/inventory/create/cloth", shopController.cloth_create_get);
+// router.get("/inventory/overview/cloth", shopController.cloth_create_get);
 
-router.get("/inventory/create/cloth", shopController.cloth_create_get);
+// router.get("/inventory/cloth", shopController.cloth_create_get);
 
 
 module.exports = router;
